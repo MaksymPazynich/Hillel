0a3ceb03a99b04780dcb0e963f9cbbc59edc8a37
Maksym Pazynich <pazynich.maksym@gmail.com>
2026-03-13 14:09:32 +0200

    додано бібліотеку chalk та приклад кольорового коду

---
+++ nodeBasics/index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue.bold('QA Engineer Max: ') + chalk.green('Проєкт налаштовано правильно!'));
+console.log(chalk.yellow.bgBlack('Тепер консоль підтримує кольоровий вивід.'));
+console.log(chalk.red.underline('Завдання виконано.'));
