e79c6f3adbc75b1985212e89e50a9d6e0be2a493
Maksym Pazynich <pazynich.maksym@gmail.com>
2026-03-18 10:34:14 +0200

    додано завдання з основ JS (типи даних, рядки, математика)

---
+++ jsBasics/task4.js
@@ -0,0 +1,16 @@
+// 4.1 Площа кола
+let radiusCircle = 5;
+let areaCircle = Math.PI * Math.pow(radiusCircle, 2);
+console.log("Площа кола:", areaCircle.toFixed(2));
+
+// 4.2 Площа прямокутника
+let length = 10;
+let width = 5.5;
+let areaRectangle = length * width;
+console.log("Площа прямокутника:", areaRectangle.toFixed(2));
+
+// 4.3 Об'єм циліндра
+let radiusCylinder = 3;
+let height = 7;
+let volumeCylinder = Math.PI * Math.pow(radiusCylinder, 2) * height;
+console.log("Об'єм циліндра:", volumeCylinder.toFixed(2));
